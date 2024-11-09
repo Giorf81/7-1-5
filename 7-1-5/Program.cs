d02bdc1e21c8a4145ed3a750211d968c1a67f0c0
Giorf81 <rejroberts7@gmail.com>
2024-11-09 13:59:01 +0300

    Добавьте файлы проекта.

---
+++ 7-1-5/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _7_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Food
+    {
+
+    }
+    class Fruits : Food
+    {
+        
+        
+    }
+    class VEgetable : Food
+    {
+
+    }
+
+    class Apple : Fruits
+    {
+
+    }
+    class Banana : Fruits
+    {
+
+    }
+
+    class Pear : Fruits
+    {
+
+    }
+
+    class Potato : VEgetable
+    {
+
+    }
+
+    class Carrot : VEgetable
+    {
+
+    }
+}
